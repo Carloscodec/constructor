6aa26dbe131827ddf1e5e4f4da948e29f2af2bd0
Carloscodec <carlosafdev@gmail.com>
2023-12-25 21:28:07 -0300

    Constructor added to class Product

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,26 +9,27 @@ public class Program {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		Product product = new Product();
 		
 		System.out.print("Product: ");
-		product.name = input.nextLine();
+		String name = input.nextLine();
 		System.out.print("Price: $");
-		product.price = input.nextDouble();
+		double price = input.nextDouble();
 		System.out.print("Quantity: ");
-		product.quantity = input.nextInt();
+		int quantity = input.nextInt();
+		
+		Product product = new Product(name, price, quantity);
 		
 		System.out.println("Product data:" + product);
 		
 		System.out.println();
 		System.out.print("Enter the amount of products to be added in stock: ");
-		int quantity = input.nextInt();
+		quantity = input.nextInt();
 		product.addProduct(quantity);
 		
 		System.out.println();
 		System.out.println("Updated data:");
 		System.out.println(product);
-	
+		
 		System.out.println();
 		System.out.print("Enter the amount of products to be removed from stock: ");
 		quantity = input.nextInt();
